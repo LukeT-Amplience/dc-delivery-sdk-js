0645313afcee4e742d2a08c818e07faefb5966c8
Luke Tyson <ltyson@amplience.com>
2021-11-10 13:54:57 +0000

    feat: manual test cusrtom retry config

--- manual-tests.ts
+++ manual-tests.ts
@@ -1,5 +1,11 @@
 import { ContentClient } from './build/main/index';
 
+import readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
 const proxy = {
   protocol: 'http',
   host: 'localhost',
@@ -21,5 +27,35 @@ cd2FreshClient.contentClient.defaults.proxy = proxy;
     console.log(error);
     return;
   }
-  console.log('\n🎉 All manual tests completed 🎉\n');
+  console.log('\n🎉 Default retry test completed 🎉\n');
+  testCustomRetries();
 });
+
+async function testCustomRetries() {
+  await rl.question('\n Enter amount of retries \n', function (retries: any) {
+    const retryNum = parseInt(retries);
+    if (isNaN(retries)) {
+      console.log('\n Must enter a number \n');
+      testCustomRetries();
+    } else {
+      const cd2FreshCustomClient: any = new ContentClient({
+        baseUrl: 'https://cdv3.fresh.content.amplience-turing.net',
+        hubName: 'cdv3',
+        apiKey: 'BEgRwAHdmL9NX8TFMnAtT6zNyAYUpqrO97NyvCfX',
+        retryConfig: { retries: retryNum },
+      });
+      cd2FreshCustomClient.contentClient.defaults.proxy = proxy;
+      (async function () {
+        await cd2FreshCustomClient.getContentItemByKey('categories');
+      })().catch((error) => {
+        if (error.status !== 429) {
+          console.log(error);
+          return;
+        }
+        console.log('\n🎉 Custom retry test completed 🎉\n');
+        rl.close();
+        process.exit(0);
+      });
+    }
+  });
+}
